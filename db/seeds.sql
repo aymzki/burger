5306b738b175286aa8696067daa1abcaf3f28837
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-11 18:36:23 -0500

    set up all the folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("kale me with kindness burger", TRUE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("camambert-ly legal burger", TRUE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("itsy bitsy teeny weeny yellow polkadot zucch
+ini burger", FALSE);
---
+++ server.js
@@ -0,0 +1,7 @@
+//Dependencies
+var express = require("express");
+//Create instance of express app
+//Set up PORT for application
+//Set Up Handlebars as Default Templating Engine
+//Data
+
