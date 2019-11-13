d588ddeff3634682d062e8e9b57851cb667d1415
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-13 06:47:17 -0500

    controller.js with routing for each method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,4 +8,35 @@ var burger = require('../models/burger.js');
 router.get("/", function(req, res) {
       res.redirect('/index');
     });
-//Exporting Router
+// selectAll
+router.get('/index', function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {burgers: data};
+		res.render('index', hbsObject);
+	});
+});
+
+
+
+// insertOne
+router.post('/burgers/insertOne', function(req, res) {
+	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
+		res.redirect('/index');
+	});
+});
+
+
+
+// updateOne
+router.put('/burgers/updateOne/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+	console.log('condition', condition);
+    //function() or function(res)?
+	burger.updateOne({devoured: req.body.devoured}, condition, function(res) {
+		res.redirect('/index');
+	});
+});
+
+
+// Exporting Router
+module.exports = router;
