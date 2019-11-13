6b0cf602ad7c1b42b46b28ebd4b10a49e76db549
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-12 23:45:52 -0500

    main.handlebars completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,11 @@
-//Import Express, and burger.js
+//Dependencies, import express, router, and burger.js
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
 //Router
+//Index redirect
+router.get("/", function(req, res) {
+      res.redirect('/index');
+    });
 //Exporting Router
