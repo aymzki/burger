90bb0ca63990a618b0fc7761562fd7fe96c63a40
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-12 21:48:13 -0500

    models burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,26 @@
 //import orm.js 
-//code that will call the ORM functions using burger specific input for the ORM.
+var orm = require("../config/orm.js");
+
+//code that will call the ORM functions using burger specific input for the ORM
+var burger = {
+    //Select All
+    selectAll: function(cb) {
+      orm.selectAll('burgers', function(res){
+          cb(res);
+      });
+    },
+    //Insert one
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne('burgers', cols, vals, function(res){
+          cb(res);
+      });
+    },
+    // Update one
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne('burgers', objColVals, condition, function(res){
+          cb(res);
+      });
+    }
+  };
 //Export at end
+module.exports = burger;
