bad4e67a6cc77e504dd0bd95ab598e9e727c90f3
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-16 20:02:59 -0500

    JAWS DB setup

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+//JAWSDB
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'diosgpxjhx4cjfp3',
+    password: 'khwr3kw1sda8nbex',
+    database: 'fcte3qncfpr2pkm7'
+  });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
